e27248bac7b89803040564676434ed558e0f1114
Ziqinzhao <60150184+Ziqinzhao@users.noreply.github.com>
2020-02-20 16:23:55 -0500

    updates

--- app/ui.r
+++ app/ui.r
@@ -1,4 +1,5 @@
 
+
 library(shiny)
 library(shinydashboard)
 ui <- dashboardPage(skin = "blue",
@@ -14,40 +15,150 @@ ui <- dashboardPage(skin = "blue",
                     dashboardBody(
                         tabItems(
                             #introduction
-                            tabItem(tabName = "Introduction",
-                                    fluidPage(
-                                      fluidRow(
-                                        box(width = 12, title = "Introduction",status = "primary",
-                                            h4("NYC Shooting Crime Map")
-                                            )),
-                                      fluidRow(
-                                        box(width = 12, title = "User Guide",status = "primary",
-                                            h4("What Does This Map Do?")
-                                            )),
-                                      fluidRow(
-                                        box(width = 4, title = "User Guide",color = "blue",
-                                            solidHeader = TRUE, 
-                                            h4("What Does This Map Do?")),
-                                        box(width = 4, title = "User Guide",color = "blue",
-                                            solidHeader = TRUE, 
-                                            h4("What Does This Map Do?")),
-                                        box(width = 4, title = "User Guide",color = "blue",
-                                            solidHeader = TRUE, 
-                                            h4("What Does This Map Do?"))),
-                                      fluidRow(
-                                        img(src = "../background.jpg",width = 580))
+                          tabItem(tabName = "Introduction",
+                                  fluidPage(
+                                    fluidRow(
+                                      box(width = 12, title = "Introduction",status = "primary",
+                                          h4("NYC Shooting Crime Map")
                                       )),
+                                    fluidRow(
+                                      box(width = 12, title = "User Guide",status = "primary",
+                                          h4("What Does This Map Do?")
+                                      )),
+                                    fluidRow(
+                                      box(width = 4, title = "User Guide",color = "blue",
+                                          solidHeader = TRUE, 
+                                          h4("What Does This Map Do?")),
+                                      box(width = 4, title = "User Guide",color = "blue",
+                                          solidHeader = TRUE, 
+                                          h4("What Does This Map Do?")),
+                                      box(width = 4, title = "User Guide",color = "blue",
+                                          solidHeader = TRUE, 
+                                          h4("What Does This Map Do?"))),
+                                    fluidRow(
+                                      img(src = "../background.jpg",width = 580))
+                                  )),
+                          
+                            
                             #map
-                            tabItem(tabName = "Map",
+                          tabItem(tabName = "Map",
+                                  fluidPage(
+                                    fluidRow(
+                                      column(3,
+                                             sliderInput("check2_pr", "Price Per Room:",min = 850, max = 5400, value = 5400)),
+                                      column(3,
+                                             sliderInput("check2_ty", "Size:",min = 10, max = 200, value = 100)),
+                                      column(3,
+                                             selectInput("check2_re", "Restaurant Type:", c("Food I Like"="",list("American", "Chinese", "Italian", "Japanese", "Pizza", "Others")), multiple=TRUE)),
+                                      column(3,
+                                             selectInput("check2_tr", "Transportation:", list("Don't Care","It's everything")))),
+                                    
+                                    fluidRow(
+                                      column(3,
+                                             selectInput(" cv ", "School nearby:",list("K-8","other"))),
+                                      
+                                      column(3,
+                                             selectInput("check2_cb", "Retail & Market:", list("Just Amazon","Important!"))),
+                                      column(3,
+                                             selectInput("check2_ct", "Crime:",list("Don't care","Safety more"))),
+                                      column(3,
+                                             selectInput("check2_ma","Noise:",list("Who cares.","No noise")))),
+                                    
+                                    fluidRow(
+                                      column(3,
+                                             h1("Choose What You Like"),
+                                             fluidRow(
+                                               column(2,
+                                                      div(id = "action",actionButton("no_rec2", "Reset"))),
+                                             )),
+                                      column(3,
+                                             selectInput("check2_cb", "Park:", list("Always stay at home","I love park!")))
                                     ),
+                                    
+                                    hr(),
+                                    
+                                    
+                                    
+                                    fluidRow(
+                                      column(6,
+                                             leafletOutput("map3", width = "auto", height = 490),
+                                             fluidRow(column(1,actionButton("click_back_buttom",label="Click here back to original view")))
+                                      ),
+                                      column(6,
+                                             dataTableOutput("recom")
+                                      )))
+                          ),
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                            #tabItem(tabName = "Map",
+                                    #),
                             
                             #details
                             tabItem(tabName = "Details",
+                                  
                                     ),
                             
                             #prediction and recommendation
                             tabItem(tabName = "Prediction and Recommendation",
+                                    fluidPage(
+                                      fluidRow(
+                                        column(3,
+                                               sliderInput("check2_pr", "Price Per Room:",min = 850, max = 5400, value = 5400)),
+                                        column(3,
+                                               selectInput("check2_ty", "Size:",c("Types I Like"="",list("Studio","1B","2B", "3B","4B")), multiple=TRUE)),
+                                        column(3,
+                                               selectInput("check2_re", "Restaurant Type:", c("Food I Like"="",list("American", "Chinese", "Italian", "Japanese", "Pizza", "Others")), multiple=TRUE)),
+                                        column(3,
+                                               selectInput("check2_tr", "Transportation:", list("Don't Care","It's everything")))),
+                                      
+                                      fluidRow(
+                                        column(3,
+                                               selectInput(" cv ", "School nearby:",list("K-8","other"))),
+                                      
+                                        column(3,
+                                               selectInput("check2_cb", "Retail & Market:", list("I'm allergic.","Drink one or two.","Let's party!"))),
+                                        column(3,
+                                               selectInput("check2_ct", "Crime:",list("Don't care","Safety more"))),
+                                        column(3,
+                                               selectInput("check2_ma","Noise:",list("Who cares.","No noise")))),
+                                    
+                                      fluidRow(
+                                        column(3,
+                                               h1("Choose What You Like"),
+                                               fluidRow(
+                                                 column(2,
+                                                        div(id = "action",actionButton("no_rec2", "Reset"))),
+                                               )),
+                                        column(3,
+                                               selectInput("check2_cb", "Park:", list("I'm allergic.","Drink one or two.","Let's party!")))
+                                        ),
+                                      
+                                      hr(),
+                                      
+                                      
+                                      
+                                      fluidRow(
+                                        column(6,
+                                               leafletOutput("map3", width = "auto", height = 490),
+                                               fluidRow(column(1,actionButton("click_back_buttom",label="Click here back to original view")))
+                                        ),
+                                        column(6,
+                                               dataTableOutput("recom")
+                                        )))
                                     ),
+                                    
+                                    
+                                    
+                                    
+                        
+                        
                             
                             #data
                             tabItem(tabName = "Data",
