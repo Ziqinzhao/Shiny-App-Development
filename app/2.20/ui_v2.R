123d17642e794e25db5d0325a0993e3d971db28a
marsyachairuna <60159136+marsyachairuna@users.noreply.github.com>
2020-02-24 01:53:09 -0500

    Update Introduction by Marsya

--- app/2.20/ui_v2.R
+++ app/2.20/ui_v2.R
@@ -23,33 +23,58 @@ shinyUI(
                           
                           mainPanel(width=12,
                                     setBackgroundImage(
-                                      src = "../background_1.jpg"
+                                      src = "https://images.squarespace-cdn.com/content/v1/5b16f1b3b27e39a160a8f256/1561760504190-W36MULRW8IWVMYCP18E0/ke17ZwdGBToddI8pDm48kDITAuO7WvGYe_HtkNx7UXUUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcQbIjqAfhDXNkvlGRG3YzfcUFWQufmCUNtpzqkVhTQlW_0KK_D1La8oUscKbt8gvv/City+-+low+opacity.png?format=2500w"
                                     ),
                                     
-                                    style = "color: white",
+                                    style = "color: black",
                                     
-                                      h2("Are Children Safe During After-school Activities?"),
-                                      h3("News:"),
-                                      h3("Dulce Maria Alavez, A 5-Year-Old Girl Went Missing From a Playground in New Jersey"),
-                                      p("Dulce went missing on the afternoon of Sept. 
-                                      16 while her mother in the car with the 8-year-old son, roughly 30 yards away. The mother is still seeking 
-                                      for information about her daughter two weeks after the girl is believed to have been abducted from a local park."),
-                                      p(em(a("New York Times",href="https://www.nytimes.com/2019/10/01/nyregion/missing-child-nj-dulce-alavez.html"))),
+                                    h2("Discover sold homes and properties to find your ideal home in Manhattan.", align = "center"),
+                                    div(class="header", "Group Project by Shuxin Chen, Junyan Guo, Xiyao Yan, Zhao Ziqin, Marsya Chairuna",align = "center"),
+                                    p(em(a("Github link",href="https://github.com/TZstatsADS/Spring2020-Project2-group3")), align = "center"),
+                                    p(""),
+                                    h3("Manhattan House Finder will help you find localize insights on every listing."),
+                                    p("Finding the process of looking for homes and properties in Manhattan inefficient? Feeling like your landlords and agents charged you unreasonably high pricing given a bare minimum specification? Feeling familiar with any one of these issues? Manhattan House Finder got you."),
+                                    p("We will help you benchmark based on discover sold homes for your home search journey based on the criteria you most care about - catered to your lifestyle."),
+                                    tags$div(tags$ul(
+                                      tags$li("Price: The sales price of a home/property"),
+                                      tags$li("Square Feet: The square footage or area, is the size of a lot or the floor space of a building or any other piece of real estate or property. "),
+                                      tags$li("School: The list or proximity of schools around the home or property, including early childhood, K-8, K-12, elementary, junior High, senior high school, etc."),
+                                      tags$li("Park: The list or proximity of famous landmark or park around the home or property."),
+                                      tags$li("Bus and Subway: The list or proximity of bus terminal or subway station around the home or property."),
+                                      tags$li("Restaurant: The list or proximity of restaurants around the home or property."),
+                                      tags$li("Retail: The list or proximity of food retails (i.e. Whole Foods, etc.) around the home or property, including small, medium, and large retails."),
+                                      tags$li("Crime: The list or proximity of pastcrimes around the home or property."),
+                                      tags$li("Noise: The list of past source of noice surrounding the area of home or property."),
+                                    )),
                                     
-                                    h2("Are Children Safe During After-school Activities?"),
-                                    p("Diego, a boy who was critically injured last week after being sucker-punched at school 
-                                      in an assault captured on video died Tuesday night. Diego appears to hit his head on a concrete.He died after sending 
-                                      to the hospital in a extremely critical condition.
-                                      "),
-                                    p(em(a("Los Angeles Times",href="https://www.latimes.com/california/story/2019-09-25/boy-sucker-punched-moreno-valley-school-dies-from-injuries"))),
+                                    h3("Target Users"),
+                                    h4("Based on price sensitivity"),
+                                    strong("The Affordable Housing and Middle-Income Tenants"),
+                                    p("Users whose utmost priority are economical price. These customers have tendency to be more flexible in terms of their non-cost selection criteria such as proximity to subway station, landmarks, schools, etc."),
+                                    strong("The Lifestyle Tenants"),
+                                    p("Established users whose priority are good services and ammenities. They are willing to pay extra to get to live in strategic residential location."),
+                                    h4("Based on location flexibility"),
+                                    strong("Flexible Tenants"),
+                                    p("Users with minimum to no preference in their house/property locations, usually as long as they get the most economical prices."),
+                                    strong("Constrained Tenants"),
+                                    p("Users who need to live specifically in certain boroughs/neighborhood because of occupation constraint."),
+                                    p(em(a("Source: Landlord.com",href="http://www.landlord.com/renter_demographics.htm"))),
                                     
-                                    h2("Are Children Safe During After-school Activities?"),
-                                    p("Developing an App that relates the crimes related to kids or teenagers with places where children spend the most, for instance, after-school activities spots. Inspired by how the Amber Alarm works, We want to develop visualization and give children warnings of certain areas even before they are in danger.
-                                      "),
-                                    p(),
-                                    p(em(a("Github link",href="https://github.com/TZstatsADS/fall2019-proj2--sec2-grp8"))),
-                                    div(class="footer", "Group Project by Lihao Xiao, Dingyi Fang, Mo Yang, Thomson Batidzirai, Sixuan Li")
-                 )),
+                                    h3("Instructions by Tab"),
+                                    h4("Map"),
+                                    p("This page features a browser map. Users can pick from multiple information filters (e.g., bus and subway station, park, restaurant, etc.) to understand the generalized information in their pinpointed areas."),
+                                    h6("Recommended for all tenants."),
+                                    h4("Recommendation"),
+                                    p("This page includes a filtering feature where users can browse the list of houses/properties recommendation based on both cost and non-cost criteria that they choose."),
+                                    h6("Recommended for Flexible and Affordable Housing Tenants."),
+                                    h4("Evaluation"),
+                                    p("This page includes a location search feature where users can type in their prospective address as an input (e.g., school address). The engine will help tenants get a summarized information of residential listings around that address."),
+                                    h6("Recommended for Constrained and Lifestyle Tenants."),
+                                    h4("Statistics"),
+                                    p("This page features a quick comparison between Manhattan neighborhoods based on the count of non-cost indexes i.e., the number of schools or parks in a particular neighborhood."),
+                                    h6("Recommended for all tenants.")
+                                    
+                          )),
                  
                  tabPanel("Map",
                           div(class="outer",
